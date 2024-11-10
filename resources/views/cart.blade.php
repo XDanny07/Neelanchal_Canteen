4beb8a2128f84ebe9ee628fae7ddb10603c7e35c
Omkar Bangar <ombangar0@gmail.com>
2024-11-11 02:28:59 +0530

    minor changes

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -140,7 +140,7 @@ if (!empty(session('cart'))) {
                 <div class="px-6 pt-3 border-t">
                     <div class="flex justify-end">
                         <button type="button" class="closeOrderModal bg-gray-700 text-gray-100 rounded px-4 py-2 mr-1">Close</Button>
-                        <button type="button" class="openPaymentModal bg-green-600 text-white rounded px-4 py-2">Proceed to Payment</Button>
+                        <button type="button" class="openPaymentModal bg-green-600 text-white rounded px-4 py-2">Proceed to Order</Button>
                     </div>
                 </div>
             </form>
@@ -170,7 +170,7 @@ if (!empty(session('cart'))) {
                         <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                     </svg>
                 </div>
-                <h3 class="mb-3 text-lg font-normal text-gray-500">Payment is being processed.</h3>
+                <h3 class="mb-3 text-lg font-normal text-gray-500">Request is being processed.</h3>
                 {{-- <button data-modal-toggle="popup-modal" type="button" class="closePaymentModal text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
                         Yes, I'm sure
                     </button>
@@ -200,7 +200,7 @@ if (!empty(session('cart'))) {
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                 </div>
-                <h3 class="mb-4 text-2xl font-semibold text-gray-600">Payment successful!</h3>
+                <h3 class="mb-4 text-2xl font-semibold text-gray-600">Order Submitted!</h3>
                 {{-- <button data-modal-toggle="popup-modal" type="button" class="closePaymentModal text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
                         Yes, I'm sure
                     </button>
